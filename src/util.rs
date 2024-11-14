ef1ff2d47668dc248960b2f86735a351257e9c1c
Oliver Tale-Yazdi <oliver@tasty.limo>
2024-11-14 15:17:37 +0700

    Server ALPN
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>

--- src/util.rs
+++ src/util.rs
@@ -13,10 +13,18 @@ pub fn configure_server() -> Result<(ServerConfig, Vec<u8>), Box<dyn Error>> {
     let priv_key = rustls::PrivateKey(priv_key);
     let cert_chain = vec![rustls::Certificate(cert_der.clone())];
 
-    let mut server_config = ServerConfig::with_single_cert(cert_chain, priv_key)?;
+    // Create ServerConfig with ALPN protocols
+    let mut server_crypto = rustls::ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(cert_chain.clone(), priv_key.clone())?;
+    
+    // Set ALPN protocols - adjust these values based on your needs
+    server_crypto.alpn_protocols = vec![b"jamnp-s/0/H".to_vec()];
+
+    let mut server_config = ServerConfig::with_crypto(Arc::new(server_crypto));
     let transport_config = Arc::get_mut(&mut server_config.transport).unwrap();
     transport_config.max_concurrent_uni_streams(0_u8.into());
-    // Set the idle timeout to higher values
     transport_config.max_idle_timeout(Some(Duration::from_secs(5 * 60).try_into().unwrap()));
 
     Ok((server_config, cert_der))
